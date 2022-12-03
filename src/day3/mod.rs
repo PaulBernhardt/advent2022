71ab61bb9ae48b85ffaa8c9942fd54b60d3ea05f
Paul Bernhardt <paul@mappedin.ca>
2022-12-03 15:26:29 -0500

    feat: day3a

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -1,41 +1,74 @@
+use std::collections::{HashMap, HashSet};
+
 pub fn solve_a(contents: &str) -> i32 {
-    let mut totals = Vec::new();
-    let mut current = 0;
+    let item_map = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        .chars()
+        .zip(1..53)
+        .collect::<HashMap<char, i32>>();
+
+    let mut total = 0;
     for line in contents.lines() {
-        let result = line.parse::<i32>();
-        match result {
-            Ok(num) => {
-                // println!("{}", num);
-                current = current + num
-            }
-            Err(_) => {
-                totals.push(current);
-                current = 0
+        let (left, right) = line.split_at(line.len() / 2);
+        let left_index = left.chars().collect::<HashSet<char>>();
+
+        for char in right.chars() {
+            if left_index.contains(&char) {
+                total = total + item_map.get(&char).unwrap();
+                break;
             }
         }
     }
-    totals.sort_unstable_by(|a, b| b.cmp(a));
 
-    return totals[0];
+    return total;
 }
 
 pub fn solve_b(contents: &str) -> i32 {
-    let mut totals = Vec::new();
-    let mut current = 0;
+    let item_map = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        .chars()
+        .zip(1..53)
+        .collect::<HashMap<char, i32>>();
+
+    let mut total = 0;
     for line in contents.lines() {
-        let result = line.parse::<i32>();
-        match result {
-            Ok(num) => {
-                // println!("{}", num);
-                current = current + num
-            }
-            Err(_) => {
-                totals.push(current);
-                current = 0
+        let (left, right) = line.split_at(line.len() / 2);
+        let left_index = left.chars().collect::<HashSet<char>>();
+
+        for char in right.chars() {
+            if left_index.contains(&char) {
+                total = total + item_map.get(&char).unwrap();
+                break;
             }
         }
     }
-    totals.sort_unstable_by(|a, b| b.cmp(a));
 
-    return totals[0] + totals[1] + totals[2];
+    return total;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_a() {
+        let contents = "\
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+        assert_eq!(solve_a(contents), 157);
+    }
+
+    #[test]
+    fn test_b() {
+        let contents = "\
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+        assert_eq!(solve_b(contents), 70);
+    }
 }
