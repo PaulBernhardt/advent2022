1e706366ad5e297dae7102a3e5a7b37e7bc366c7
Paul Bernhardt <paul@mappedin.ca>
2022-12-10 13:30:26 -0500

    feat: day 8 a, plus fix typo

--- src/day8/mod.rs
+++ src/day8/mod.rs
@@ -1,19 +1,92 @@
-type Forest = Vec<Vec<u32>>;
+use std::collections::HashSet;
+
+type Forest = Vec<Vec<i32>>;
 
 fn parse_input(contents: &str) -> Result<Forest, String> {
     let mut forest: Forest = Vec::new();
     for line in contents.lines() {
-        let trees: Vec<u32> = line
+        let trees: Vec<i32> = line
             .chars()
-            .map(|c| c.to_digit(10).unwrap_or_default())
+            .map(|c| i32::try_from(c.to_digit(10).unwrap_or_default()).unwrap_or_default())
             .collect();
         forest.push(trees);
     }
     Ok(forest)
 }
 
-pub fn solve_a(_contents: &str) -> Result<String, String> {
-    Err("Not implemented".to_string())
+pub fn solve_a(contents: &str) -> Result<String, String> {
+    let trees = parse_input(contents)?;
+    let mut visible: HashSet<(usize, usize)> = HashSet::new();
+    for r in 0..trees.len() {
+        let mut max = -1;
+        let row = &trees[r];
+        for c in 0..row.len() {
+            let tree = row[c];
+            // println!("++Checking {r},{c} ({})", tree);
+            if tree > max {
+                visible.insert((r, c));
+                max = tree;
+                // println!("Fits");
+                if tree >= 9 {
+                    break;
+                }
+            }
+        }
+        max = -1;
+        for cp in 0..row.len() {
+            let c = row.len() - cp - 1;
+            let tree = row[c];
+            // println!("+-Checking {r},{c} ({})", tree);
+
+            if tree > max {
+                visible.insert((r, c));
+                max = tree;
+                // println!("Fits");
+                if tree >= 9 {
+                    break;
+                }
+            }
+        }
+    }
+
+    let row_len = trees[0].len();
+    let col_len = trees.len();
+    for c in 0..col_len {
+        let mut max = -1;
+        for r in 0..row_len {
+            //r = row_len - rp;
+            let tree = trees[r][c];
+            // println!("-+Checking {r},{c} ({})", tree);
+
+            if tree > max {
+                visible.insert((r, c));
+                max = tree;
+                // println!("Fits");
+
+                if tree >= 9 {
+                    break;
+                }
+            }
+        }
+
+        max = -1;
+        for rp in 0..row_len {
+            let r = row_len - rp - 1;
+            let tree = trees[r][c];
+            // println!("--Checking {r},{c} ({})", tree);
+
+            if tree > max {
+                visible.insert((r, c));
+                max = tree;
+                // println!("Fits");
+
+                if tree >= 9 {
+                    break;
+                }
+            }
+        }
+    }
+    Ok(format!("{}", visible.len()))
 }
 
 pub fn solve_b(_contents: &str) -> Result<String, String> {
