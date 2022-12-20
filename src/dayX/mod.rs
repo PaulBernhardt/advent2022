e86bcb964e1e6c895a7575d8731c841747e64565
Paul Bernhardt <paul@mappedin.ca>
2022-12-20 13:49:31 -0500

    feat: day13a

--- src/dayX/mod.rs
+++ src/dayX/mod.rs
@@ -12,30 +12,30 @@ mod tests {
 
     #[test]
     fn test_a() {
-        let contents = "    [D]    
-[N] [C]    
-[Z] [M] [P]
- 1   2   3 
-
-move 1 from 2 to 1
-move 3 from 1 to 3
-move 2 from 2 to 1
-move 1 from 1 to 2";
+        let contents = "\
+R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2";
         let result = solve_a(contents);
         assert_eq!(result.unwrap(), "CMZ");
     }
 
     #[test]
     fn test_b() {
-        let contents = "    [D]    
-[N] [C]    
-[Z] [M] [P]
- 1   2   3 
-
-move 1 from 2 to 1
-move 3 from 1 to 3
-move 2 from 2 to 1
-move 1 from 1 to 2";
+        let contents = "\
+R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2";
         let result = solve_b(contents);
         assert_eq!(result.unwrap(), "CMZ");
     }
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ mod day1;
 mod day10;
 mod day11;
 mod day12;
+mod day13;
 mod day2;
 mod day3;
 mod day4;
@@ -180,6 +181,22 @@ pub fn run() -> Result<(), Box<dyn Error>> {
     };
     let time = Instant::now();
     let b = day12::solve_b(&contents);
+    match b {
+        Ok(b) => println!("  b: {} ({:?})", b, time.elapsed()),
+        Err(b) => println!("  b: {}", b),
+    };
+    println!();
+
+    let contents = fs::read_to_string("./input/day13.txt")?;
+    println!("13:");
+    let time = Instant::now();
+    let a = day13::solve_a(&contents);
+    match a {
+        Ok(a) => println!("  a: {} ({:?})", a, time.elapsed()),
+        Err(a) => println!("  a: {}", a),
+    };
+    let time = Instant::now();
+    let b = day13::solve_b(&contents);
     match b {
         Ok(b) => println!("  b: {} ({:?})", b, time.elapsed()),
         Err(b) => println!("  b: {}", b),
